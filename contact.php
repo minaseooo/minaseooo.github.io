e799697f314d1e56f3700358fd69868162d2061b
minaseooo <riririnose@gmail.com>
2023-03-26 16:30:22 +0900

    update

--- contact.php
+++ contact.php
@@ -1,96 +1,38 @@
-<?php 
-    // フォームのボタンが押されたら
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // フォームから送信されたデータを各変数に格納
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $content  = $_POST["content"];
+<!-- ロジック
+================================================================================================ -->
+<?php
+// セッション開始
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //フォームのボタンが押されたら、POSTされたデータを各変数に格納
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // トークンの生成（CSRF対策）
+    $token = bin2hex(random_bytes(32));
+    $_SESSION['token'] = $token;
+
+    // HTML出力前のエスケープ処理
+    function escape($str)
+    {
+        return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
     }
-
-    // 送信ボタンが押されたら
-    if (isset($_POST["submit"])) {
-        // 送信ボタンが押された時に動作する処理をここに記述する
-            
-        // 日本語をメールで送る場合のおまじない
-            mb_language("ja");
-        mb_internal_encoding("UTF-8");
-        
-        //mb_send_mail("kanda.it.school.trial@gmail.com", "メール送信テスト", "メール本文");
-
-            // 件名を変数subjectに格納
-            $subject = "［自動送信］お問い合わせ内容の確認";
-
-            // メール本文を変数bodyに格納
-        $body = <<< EOM
-{$name}　様
-
-お問い合わせありがとうございます。
-
-===================================================
-【 お名前 】 
-{$name}
-
-【 メール 】 
-{$email}
-
-【 内容 】 
-{$content}
-===================================================
-
-内容を確認のうえ、回答させて頂きます。
-しばらくお待ちください。
-EOM;
-
-
-        $subbody = <<< EOM
-{$name}　様
-
-お問い合わせいただきました。
-
-===================================================
-【 お名前 】 
-{$name}
-
-【 メール 】 
-{$email}
-
-【 お問い合わせ内容 】 
-{$contentbox}
-===================================================
-EOM;
-        
-        // 送信元のメールアドレスを変数fromEmailに格納
-		//HP作成側のメールアドレスを記述
-        $fromEmail = "sanasana.ma124@gmail.com";
-
-        // 送信元の名前を変数fromNameに格納
-		//HP作成側の名前
-        $fromName = "minase";
-
-        // ヘッダ情報を変数headerに格納する    
-		//  
-        $header = "From: " .mb_encode_mimeheader($fromName) ."<{$fromEmail}>";
-		//
-		$subheader = "From: " .mb_encode_mimeheader($name) ."<{$email}>";
-
-		//Return-Path
-		$parameter="-f $fromEmail";
-
-        // 送信者へメール送信を行う
-        mb_send_mail($email, $subject, $body, $header, $parameter);
-		// 受信者へメール送信を行う
-        mb_send_mail($fromEmail, $subject, $subbody, $subheader, $parameter);  
-
-        // サンクスページに画面遷移させる
-        header("Location: http://minaseooo.github.io/thanks.html");
-        exit;
-    }
+} else {
+    //フォームボタン以外からこのページにアクセスした場合（URL直接入力など）、トップページに戻る
+    header(("location: alert.php"));
+    exit;
+}
 ?>
+
+<!-- ビュー
+================================================================================================ -->
 <html lang="ja">
 <head>
-<meta charset="UTF-8">
-<title>お問い合わせフォーム</title>
-<link rel="stylesheet" href="css/contact.css">
+    <meta charset="UTF-8">
+    <title>確認画面</title>
+    <link rel="stylesheet" href="css/contact.css">
 </head>
 <body>
 	<div id="container">
@@ -101,33 +43,32 @@ EOM;
             </div>
 
             <div class="inner">
-            <form action="thanks.html" method="post">
-                    <input type="hidden" name="name" value="<?php echo $name; ?>">
-                    <input type="hidden" name="email" value="<?php echo $email; ?>">
-                    <input type="hidden" name="content" value="<?php echo $content; ?>">
-                    <h1 class="contact-title">内容確認</h1>
-                    <p>お問い合わせ内容はこちらで宜しいでしょうか？<br>よろしければ「送信する」ボタンを押して下さい。</p>
-                    <div class="confirm">
-                        <div>
-                            <label>お名前</label>
-                            <p><?php echo $name; ?></p>
-                        </div>
-                        <div>
-                            <label>メールアドレス</label>
-                            <p><?php echo $email; ?></p>
-                        </div>
-                        <div>
-                            <label>お問い合わせ内容</label>
-                            <p><?php echo nl2br($content); ?></p>
-                        </div>
-                    </div>
-                
-                <div class="button">
-                    <input type="button" value="内容を修正する" onclick="history.back(-1)">
-                    <button type="submit" name="submit">送信する</button>
+            <p class="confirm-text">下記の内容でメッセージを送信します。<br>よろしければ「送信する」ボタンを押してください。</p>
+            
+            <form action="thanks.php" method="post">
+                <input type="hidden" name="token" value="<?php echo escape($token); ?>">
+                <div class="contact-form">
+                    <label for="name">お名前</label>
+                    <input type="hidden" id="name" name="name" value="<?php echo escape($name); ?>">
+                    <p><?php echo escape($name); ?></p>
+                </div>
+                <div class="contact-form">
+                    <label for="email">メールアドレス</label>
+                    <input type="hidden" id="email" name="email" value="<?php echo escape($email); ?>">
+                    <p><?php echo escape($email); ?></p>
+                </div>
+                <div class="contact-form">
+                    <label for="message">メッセージ</label>
+                    <input type="hidden" id="message" name="message" value="<?php echo escape($message); ?>">
+                    <p><?php echo nl2br(escape($message)); ?></p>
                 </div>
 
+            <div class="button">
+                <input class="btn" type="button" value="前画面へ戻る" onclick="history.back(-1)">
+                <input class="btn" type="submit" value="送信する" name="submit"></input>
+            </div>
             </form>
+
             </div>
 
         </div>
---
+++ thanks.php
@@ -0,0 +1,127 @@
+<!-- ロジック
+================================================================================================ -->
+<?php
+session_start();
+
+/* 以下、メール送信の処理
+------------------------------------------------------------------------------------------------- */
+// 送信ボタンが押されたら
+if (!empty($_SESSION['token']) && $_POST['token'] === $_SESSION['token']) {
+    // //フォームのボタンが押されたら、POSTされたデータを各変数に格納
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // メールの言語設定
+    mb_language("ja");
+    mb_internal_encoding("UTF-8");
+
+    // 件名を変数subjectに格納
+    $subject = "［自動送信］メッセージ内容の確認";
+
+    // メール本文を変数bodyに格納
+    $body = <<< EOM
+  {$name}　様
+
+  メッセージありがとうございます。
+  以下の内容でメッセージを承りました。
+
+  ===================================================
+  < お名前 >
+  {$name}
+
+  < メールアドレス >
+  {$email}
+
+  < メッセージ >
+  {$message}
+  ===================================================
+
+  ※当メールは送信専用となっております。
+  　ご返信いただいても、お答えいたしかねますのでご了承ください。
+  EOM;
+
+    // 送信元のメールアドレスを変数fromEmailに格納(本番環境へのデプロイ時に正規のアドレスに変更すること！)
+    $fromEmail = "sanasana.ma124@gmail.com";
+
+    // 送信元の名前を変数fromNameに格納
+    $fromName = "Memorie Portfolio";
+
+    // ヘッダ情報を変数headerに格納する
+    $header = "From: " . mb_encode_mimeheader($fromName) . "<{$fromEmail}>";
+
+    // 受信用のメールアドレスを変数myEmailに格納(本番環境へのデプロイ時に正規のアドレスに変更すること！)
+    $myEmail = "sanasana.ma124@gmail.com";
+
+    //Return-Path
+	$parameter="-f $fromEmail";
+
+    // フォーム入力者へメールを送信する
+    mb_send_mail($email, $subject, $body, $header, $parameter);
+
+    // サイト管理者へメールを送信する
+    mb_send_mail($myEmail, $subject, $body, $header, $parameter);
+} else {
+    // トークンが一致しない場合、不正アクセス画面へ移動する
+    header(("location: alert.php"));
+    exit;
+}
+?>
+
+
+<!-- ビュー
+================================================================================================ -->
+
+<html lang="js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="ポートフォリオ">
+    <meta name="description" content="Memorie">
+    <meta name="auther" content="Memorie">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Memorie</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <!--FontAwesome--><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+   <!--Style-css--><link rel="stylesheet" href="css/thanks.css">
+</head>
+
+<body>
+
+<div id="container">
+  <div class="thanks">
+
+      <div class="MainVisual">
+        <img src="img/memoire02.png">
+      </div>
+
+        <div class="inner">
+          <div id="thanks">
+            <div class="message">
+                <h1>お問い合わせありがとうございました。</h1>
+                <p>送信が完了しました。</p>
+                <p>1週間以内に記載頂いたメールアドレスに返信いたします。</p>
+                <p>1週間経ってもお返事が届かない場合は恐れ入りますが、<br>メールフォームまたはメールにて再度お問い合わせください。</p>
+            </div><!--message-->
+          </div>
+        </div>
+
+        <div class="top-button">
+          <div class="top-page">
+            <a href="https://minaseooo.github.io">
+              トップページへ戻る
+            </a></div>
+        </div>
+
+      <footer>
+        <small>Copylight 2023 ami matsuba all right reserved.</small>
+      </footer>
+
+  </div>
+</div><!--container-->
+  
+
+
+      <!-- jQuery--><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <!-- 住所自動入力ライブラリ--><script type="text/javascript" src="js/ajaxzip3.js"></script>
+      <script src="js/script.js"></script>
+</body>
